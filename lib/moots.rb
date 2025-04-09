8b4a5117c772542e4e382005c45d8b9248d3711b
Dave Russell <dave.kerr@gmail.com>
2025-04-09 01:28:36 +0100

    Initial bundler gem boilterplate

---
+++ lib/moots.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "moots/version"
+
+module Moots
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/moots_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Moots do
+  it "has a version number" do
+    expect(Moots::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
